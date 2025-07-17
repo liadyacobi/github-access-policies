16a55771942e784210fff0b66e35fe29e4489ffb
Liad Yacobi <liadyacobi@gmail.com>
2025-07-17 13:09:30 +0300

    Implemented Github Client, fetches repos, users, teams, branches

--- go.mod
+++ go.mod
@@ -1,15 +1,18 @@
 module github.com/liadyacobi/github-access-policies
 
-go 1.23
+go 1.23.0
 
 toolchain go1.23.11
 
 require (
+	github.com/google/go-github/v57 v57.0.0
+	golang.org/x/oauth2 v0.30.0
 	google.golang.org/grpc v1.74.0
 	google.golang.org/protobuf v1.36.6
 )
 
 require (
+	github.com/google/go-querystring v1.1.0 // indirect
 	golang.org/x/net v0.40.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/text v0.25.0 // indirect
---
+++ internal/githubclient/client.go
@@ -0,0 +1,136 @@
+package githubclient
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/google/go-github/v57/github"
+	"golang.org/x/oauth2"
+)
+
+type Client struct {
+	client *github.Client
+}
+
+func NewClient(token string) *Client {
+	ctx := context.Background()
+	tokenSource := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	oauthClient := oauth2.NewClient(ctx, tokenSource)
+
+	return &Client{
+		client: github.NewClient(oauthClient),
+	}
+}
+
+func (client *Client) ListOrganizationRepositories(ctx context.Context, org string) ([]*github.Repository, error) {
+	log.Printf("Fetching repositories for organization: %s", org)
+
+	reqOpts := &github.RepositoryListByOrgOptions{
+		ListOptions: github.ListOptions{
+			PerPage: 100,
+		},
+	}
+
+	var allRepos []*github.Repository
+	for {
+		repos, resp, err := client.client.Repositories.ListByOrg(ctx, org, reqOpts)
+		if err != nil {
+			return nil, fmt.Errorf("failed to list repositories: %w", err)
+		}
+
+		allRepos = append(allRepos, repos...)
+
+		if resp.NextPage == 0 {
+			break
+		}
+		reqOpts.Page = resp.NextPage
+	}
+
+	return allRepos, nil
+}
+
+func (client *Client) GetRepository(ctx context.Context, org, repoName string) (*github.Repository, error) {
+	log.Printf("Fetching repository: %s/%s", org, repoName)
+
+	repo, _, err := client.client.Repositories.Get(ctx, org, repoName)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get repository: %w", err)
+	}
+
+	return repo, nil
+}
+
+func (client *Client) ListRepositoryCollaborators(ctx context.Context, org, repo string) ([]*github.User, error) {
+	opts := &github.ListCollaboratorsOptions{
+		ListOptions: github.ListOptions{
+			PerPage: 100,
+		},
+	}
+
+	var allCollaborators []*github.User
+	for {
+		collaborators, resp, err := client.client.Repositories.ListCollaborators(ctx, org, repo, opts)
+		if err != nil {
+			return nil, fmt.Errorf("failed to list collaborators: %w", err)
+		}
+
+		allCollaborators = append(allCollaborators, collaborators...)
+
+		if resp.NextPage == 0 {
+			break
+		}
+		opts.Page = resp.NextPage
+	}
+
+	return allCollaborators, nil
+}
+
+func (client *Client) GetRepositoryPermissionLevel(ctx context.Context, org, repo, username string) (*github.RepositoryPermissionLevel, error) {
+	permission, _, err := client.client.Repositories.GetPermissionLevel(ctx, org, repo, username)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get permission level: %w", err)
+	}
+	return permission, nil
+}
+
+func (client *Client) ListRepositoryTeams(ctx context.Context, org, repo string) ([]*github.Team, error) {
+	opts := &github.ListOptions{
+		PerPage: 100,
+	}
+
+	var allTeams []*github.Team
+	for {
+		teams, resp, err := client.client.Repositories.ListTeams(ctx, org, repo, opts)
+		if err != nil {
+			return nil, fmt.Errorf("failed to list teams: %w", err)
+		}
+
+		allTeams = append(allTeams, teams...)
+
+		if resp.NextPage == 0 {
+			break
+		}
+		opts.Page = resp.NextPage
+	}
+
+	return allTeams, nil
+}
+
+func (client *Client) ListRepositoryBranches(ctx context.Context, org, repo string) ([]*github.Branch, error) {
+	branches, _, err := client.client.Repositories.ListBranches(ctx, org, repo, &github.BranchListOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to list branches: %w", err)
+	}
+	return branches, nil
+}
+
+func (client *Client) GetBranchProtection(ctx context.Context, org, repo, branch string) (*github.Protection, error) {
+	protection, _, err := client.client.Repositories.GetBranchProtection(ctx, org, repo, branch)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get branch protection: %w", err)
+	}
+	return protection, nil
+}
